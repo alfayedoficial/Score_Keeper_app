a8f992f467ea2289f256f3b1921fb47166de7db7
alialfayed <alialfayed1@gmail.com>
2020-04-18 15:41:01 +0200

    Add landscape layout and add save instance value

--- app/src/main/java/com/zagazig/scorekeeperapp/MainActivity.java
+++ app/src/main/java/com/zagazig/scorekeeperapp/MainActivity.java
@@ -1,20 +1,17 @@
 package com.zagazig.scorekeeperapp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import icepick.Icepick;
-import icepick.State;
-
 import android.app.AlertDialog;
-import android.app.AlertDialog.Builder;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import icepick.Icepick;
+import icepick.State;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -24,17 +21,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button btnTeamB_1Point;
     private Button btnTeamB_5Points;
     private Button btnTeamB_Foul;
-    private Button btn_EndGame;
 
     private TextView teamAScoreNumber;
     private TextView teamAFailNumber;
     private TextView teamBScoreNumber;
     private TextView teamBFailNumber;
 
-    @State int SCORE_NUMBER_TEAM_A ;    // These will be automatically saved and restored
-    @State int FAIL_NUMBER_TEAM_A ;
-    @State int SCORE_NUMBER_TEAM_B ;
-    @State int FAIL_NUMBER_TEAM_B ;
+    @State
+    int SCORE_NUMBER_TEAM_A;    // These will be automatically saved and restored
+    @State
+    int FAIL_NUMBER_TEAM_A;
+    @State
+    int SCORE_NUMBER_TEAM_B;
+    @State
+    int FAIL_NUMBER_TEAM_B;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,19 +53,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void initVariable() {
         //initialize  Buttons
-        btnTeamA_1Point =  findViewById(R.id.btnTeamA_1Point);
-        btnTeamA_5Points =  findViewById(R.id.btnTeamA_5Points);
-        btnTeamA_Foul =  findViewById(R.id.btnTeamA_Foul);
-        btnTeamB_1Point =  findViewById(R.id.btnTeamB_1Point);
-        btnTeamB_5Points =  findViewById(R.id.btnTeamB_5Points);
-        btnTeamB_Foul =  findViewById(R.id.btnTeamB_Foul);
-        btn_EndGame = findViewById(R.id.btn_EndGame);
+        btnTeamA_1Point = findViewById(R.id.btnTeamA_1Point);
+        btnTeamA_5Points = findViewById(R.id.btnTeamA_5Points);
+        btnTeamA_Foul = findViewById(R.id.btnTeamA_Foul);
+        btnTeamB_1Point = findViewById(R.id.btnTeamB_1Point);
+        btnTeamB_5Points = findViewById(R.id.btnTeamB_5Points);
+        btnTeamB_Foul = findViewById(R.id.btnTeamB_Foul);
+        Button btn_EndGame = findViewById(R.id.btn_EndGame);
 
         //initialize TextView
-        teamAScoreNumber =  findViewById(R.id.teamAScoreNumber);
-        teamAFailNumber =  findViewById(R.id.teamAFailNumber);
-        teamBScoreNumber =  findViewById(R.id.teamBScoreNumber);
-        teamBFailNumber =  findViewById(R.id.teamBFailNumber);
+        teamAScoreNumber = findViewById(R.id.teamAScoreNumber);
+        teamAFailNumber = findViewById(R.id.teamAFailNumber);
+        teamBScoreNumber = findViewById(R.id.teamBScoreNumber);
+        teamBFailNumber = findViewById(R.id.teamBFailNumber);
 
         // onclick Buttons
         btnTeamA_1Point.setOnClickListener(this);
@@ -78,21 +78,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     // display Button When another player playing ...
-    private void displayPlayerRole(Boolean status){
+    private void displayPlayerRole(Boolean status) {
         btnTeamA_1Point.setEnabled(status);
         btnTeamA_5Points.setEnabled(status);
         btnTeamA_Foul.setEnabled(status);
         btnTeamA_1Point.setBackground(getDrawable(R.drawable.btn_player));
         btnTeamA_5Points.setBackground(getDrawable(R.drawable.btn_player));
         btnTeamA_Foul.setBackground(getDrawable(R.drawable.btn_player));
-        if (status){
+        if (status) {
             btnTeamB_1Point.setEnabled(false);
             btnTeamB_5Points.setEnabled(false);
             btnTeamB_Foul.setEnabled(false);
             btnTeamB_1Point.setBackground(getDrawable(R.drawable.btn_unplayer));
             btnTeamB_5Points.setBackground(getDrawable(R.drawable.btn_unplayer));
             btnTeamB_Foul.setBackground(getDrawable(R.drawable.btn_unplayer));
-        }else {
+        } else {
             btnTeamB_1Point.setEnabled(true);
             btnTeamB_5Points.setEnabled(true);
             btnTeamB_Foul.setEnabled(true);
@@ -108,7 +108,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         int id = view.getId();
-        switch (id){
+        switch (id) {
             case R.id.btnTeamA_1Point:
                 setNewScoreTeamA(1);
                 displayPlayerRole(false);
@@ -140,11 +140,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 int teamB = Integer.parseInt(valueTeamB.toString());
 
                 String winner;
-                if (teamA != 0 || teamB != 0){
-                    if (teamA > teamB){
-                        winner = "Team A" ;
-                    }else {
-                        winner = "Team B" ;
+                if (teamA != 0 || teamB != 0) {
+                    if (teamA > teamB) {
+                        winner = "Team A";
+                    } else {
+                        winner = "Team B";
                     }
                     AlertDialog.Builder dialog = new AlertDialog.Builder(this);
                     dialog.setTitle(getString(R.string.title_dialog));
@@ -159,7 +159,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     });
                     AlertDialog builder = dialog.create();
                     builder.show();
-                }else {
+                } else {
                     AlertDialog.Builder dialog = new AlertDialog.Builder(this);
                     dialog.setTitle(getString(R.string.title_dialog_two));
                     dialog.setMessage(getString(R.string.message_dialog_two));
@@ -180,7 +180,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     // method to increase score TeamA
-    private void setNewScoreTeamA(int score){
+    private void setNewScoreTeamA(int score) {
         CharSequence value = teamAScoreNumber.getText();
         int currentScore = Integer.parseInt(value.toString());
         SCORE_NUMBER_TEAM_A = currentScore + score;
@@ -189,7 +189,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     // method to increase fail TeamA
-    private void setNewFailTeamA(){
+    private void setNewFailTeamA() {
         CharSequence value = teamAFailNumber.getText();
         int currentScore = Integer.parseInt(value.toString());
         int FAIL_NUMBER_TEAM_A = currentScore + 1;
@@ -198,7 +198,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     // method to increase score TeamA
-    private void setNewScoreTeamB(int score){
+    private void setNewScoreTeamB(int score) {
         CharSequence value = teamBScoreNumber.getText();
         int currentScore = Integer.parseInt(value.toString());
         SCORE_NUMBER_TEAM_B = currentScore + score;
@@ -207,7 +207,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     // method to increase fail TeamA
-    private void setNewFailTeamB(){
+    private void setNewFailTeamB() {
         CharSequence value = teamBFailNumber.getText();
         int currentScore = Integer.parseInt(value.toString());
         FAIL_NUMBER_TEAM_B = currentScore + 1;
@@ -216,17 +216,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     // method to set App Default
-    private void setDefault(){
+    private void setDefault() {
         teamAScoreNumber.setText("0");
         teamAFailNumber.setText("0");
         teamBScoreNumber.setText("0");
         teamBFailNumber.setText("0");
+
+        SCORE_NUMBER_TEAM_A = 0;
+        FAIL_NUMBER_TEAM_A = 0;
+        SCORE_NUMBER_TEAM_B = 0;
+        FAIL_NUMBER_TEAM_B = 0;
+
         btnTeamB_1Point.setEnabled(true);
         btnTeamB_5Points.setEnabled(true);
         btnTeamB_Foul.setEnabled(true);
         btnTeamA_1Point.setEnabled(true);
         btnTeamA_5Points.setEnabled(true);
         btnTeamA_Foul.setEnabled(true);
+
         btnTeamB_1Point.setBackground(getDrawable(R.drawable.btn_player));
         btnTeamB_5Points.setBackground(getDrawable(R.drawable.btn_player));
         btnTeamB_Foul.setBackground(getDrawable(R.drawable.btn_player));
